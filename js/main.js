879c69d6ba881da19336ef4420d342803708fcc6
gilvanfcf77 <gilvanfcf77@gmail.com>
2016-12-02 14:51:39 -0300

    função localização adicionada

--- js/main.js
+++ js/main.js
@@ -214,21 +214,32 @@ jQuery(function($) {'use strict';
 	});
 
 	//Google Map
-	var latitude = $('#google-map').data('latitude');
-	var longitude = $('#google-map').data('longitude');
-	function initialize_map() {
-		var myLatlng = new google.maps.LatLng(latitude,longitude);
-		var mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: myLatlng
-		};
-		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-	}
-	google.maps.event.addDomListener(window, 'load', initialize_map);
+	 function initMap() {
+        var uluru = {lat: -3.804694, lng: -38.572667};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 18,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+     }
+	// var latitude = $('#google-map').data('latitude');
+	// var longitude = $('#google-map').data('longitude');
+	// function initialize_map() {
+	// 	var myLatlng = new google.maps.LatLng(latitude,longitude);
+	// 	var mapOptions = {
+	// 		zoom: 14,
+	// 		scrollwheel: false,
+	// 		center: myLatlng
+	// 	};
+	// 	var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+	// 	var marker = new google.maps.Marker({
+	// 		position: myLatlng,
+	// 		map: map
+	// 	});
+	// }
+	// google.maps.event.addDomListener(window, 'load', initialize_map);
 
 });
