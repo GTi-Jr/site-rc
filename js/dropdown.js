9f8d477b0299881d87b7b6d142ab0102b33e3260
gilvanfcf77 <gilvanfcf77@gmail.com>
2016-12-17 20:11:50 -0300

    clean code

--- js/dropdown.js
+++ js/dropdown.js
@@ -6,30 +6,9 @@ function dropDownProducts() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.dropbtn') || event.target.matches('.scroll')) {
 
     var dropdowns = document.getElementsByClassName("product-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function dropDownLanguages() {
-    document.getElementById("languages").classList.toggle("show");
- }
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("language-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
