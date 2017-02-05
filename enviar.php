427979493b7df2baad8acc77d278204ecd5721a3
gilvanfcf77 <gilvanfcf77@gmail.com>
2017-02-05 16:22:27 -0300

    formulário de contato adicionadogit statusgit status

---
+++ enviar.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome=$_POST[nome];
+$email=$_POST[email];
+$assunto=$_POST[assunto];
+$mensagem=$_POST[mensagem];
+
+mail("gilvanfcf77@gmail.com","$assunto","
+Nome: $nome
+Email: $email
+Assunto: $assunto
+Mensagem: $mensagem","FROM:$nome<$email>");
+
+echo "Sua mensagem foi enviada com sucesso!";
+
+?>
